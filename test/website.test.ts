fed99a9aa13a2b2a475aa26f703054c2e533b871
Sebastian Kropp <sebastian@kropponline.de>
2018-10-09 00:35:27 -0400

    fix(transition$): not emitting null

---
+++ test/website.test.ts
@@ -0,0 +1,83 @@
+import { FSM } from '../src/ea-state-machine'
+import { State, StateMap, TransitionDefinitionMap } from '../src/types'
+
+const stepStateMachine = () => {
+  const startState: State = {
+    name: 'Start State',
+    path: '',
+    component: { name: 'Start Component' },
+    onEnter: fsm => {
+      console.log(fsm.currentState.name + ' entered')
+    },
+  }
+
+  const state: StateMap = {
+    application: {
+      name: 'Application View',
+      path: 'application/:id',
+      component: { name: 'Application View Component' },
+      onEnter: fsm => {
+        console.log(fsm.currentState.name + ' entered')
+      },
+      onExit: fsm => {
+        console.log(fsm.currentState.name + ' exited')
+      },
+    },
+    entity: {
+      name: 'Entity View',
+      path: 'entity/:id',
+      component: { name: 'Entity View Component' },
+      onEnter: fsm => {
+        console.log(fsm.currentState.name + ' entered')
+      },
+      onExit: fsm => {
+        console.log(fsm.currentState.name + ' exited')
+      },
+    },
+    document: {
+      name: 'Document View',
+      path: 'document/:id',
+      component: { name: 'Entity View Component' },
+      onEnter: fsm => {
+        console.log(fsm.currentState.name + ' entered')
+      },
+      onExit: fsm => {
+        console.log(fsm.currentState.name + ' exited')
+      },
+    },
+  }
+
+  const anyState: State[] = Object.values(state)
+
+  const transitionDefinition: TransitionDefinitionMap = {
+    initialize: {
+      from: () => startState,
+      to: () => anyState,
+      action: () => console.log('starting the statemachine'),
+    },
+    any: {
+      from: anyState,
+      to: anyState,
+      action: () => console.log('any transition'),
+    },
+  }
+
+  return Object.freeze({ startState, state, transitionDefinition })
+}
+
+describe('Website', () => {
+  const { startState, state, transitionDefinition } = stepStateMachine()
+  const fsm = new FSM(state, transitionDefinition, startState)
+
+  test('transition$ emits transitions', done => {
+    expect(fsm.currentState).toEqual(startState)
+    fsm.transition$.subscribe(t => {
+      expect(t).toBeTruthy()
+      if (t.transitionDefinition === transitionDefinition.initialize) {
+        expect(t.toState).toBe(state.application)
+      }
+      done()
+    })
+    fsm.transitionTo(state.application)
+  })
+})
